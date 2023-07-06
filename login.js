b4ff7114c519523d5666f9187654f7c0fd622d6a
LuisEstela <70233033+LuisEstela@users.noreply.github.com>
2023-07-06 06:39:28 -0400

    Actualizacion inicio de sesion (LOCAL STORAGE)
    +SE AÑADIERON METODOS PARA ALMACENAR EL UID DE USUARIO EN EL LOCALSTORAGE

--- login.js
+++ login.js
@@ -1,41 +1,46 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDrzCRyT3S-SY7Vvh58jkPtstVSLMvsStQ",
-    authDomain: "gestorinventarios-e37f6.firebaseapp.com",
-    databaseURL: "https://gestorinventarios-e37f6-default-rtdb.firebaseio.com",
-    projectId: "gestorinventarios-e37f6",
-    storageBucket: "gestorinventarios-e37f6.appspot.com",
-    messagingSenderId: "968579423727",
-    appId: "1:968579423727:web:04d75d7fda53cfd8086a1a"
-  };
+  apiKey: "AIzaSyDrzCRyT3S-SY7Vvh58jkPtstVSLMvsStQ",
+  authDomain: "gestorinventarios-e37f6.firebaseapp.com",
+  databaseURL: "https://gestorinventarios-e37f6-default-rtdb.firebaseio.com",
+  projectId: "gestorinventarios-e37f6",
+  storageBucket: "gestorinventarios-e37f6.appspot.com",
+  messagingSenderId: "968579423727",
+  appId: "1:968579423727:web:04d75d7fda53cfd8086a1a",
+};
 
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
 
-  firebase.initializeApp(firebaseConfig);
-  var auth = firebase.auth();
-  
-  function loginUser() {
-    var usuario = document.getElementById("usuario").value;
-    var contrasena = document.getElementById("contrasena").value;
-  
-    // Verify that both fields are filled
-    if (usuario === "" || contrasena === "") {
-      alert("Rellene todos los campos.");
-      return;
-    }
-  
-    auth.signInWithEmailAndPassword(usuario, contrasena)
-      .then(function (userCredential) {
-        // Login successful
-        console.log("User logged in:", userCredential.user);
-        // Redirect to another page after successful login
-        window.location.href = "MenuPrincipal/menuprincipal.html";
-      })
-      .catch(function (error) {
-        // Login error
-        console.log("Login error:", error.code, error.message);
-        alert("Inicio de sesión fallido. Verifique sus credenciales.");
-      });
+function loginUser() {
+  var usuario = document.getElementById("usuario").value;
+  var contrasena = document.getElementById("contrasena").value;
+
+  // Verify that both fields are filled
+  if (usuario === "" || contrasena === "") {
+    alert("Rellene todos los campos.");
+    return;
   }
-  
-  // Add an event listener to the login button
-  document.getElementById("login-btn").addEventListener("click", loginUser);
+
+  auth
+    .signInWithEmailAndPassword(usuario, contrasena)
+    .then(function (userCredential) {
+      // Login successful
+      var user = userCredential.user;
+      console.log("User logged in:", user);
+
+      // Store the UID in localStorage
+      localStorage.setItem("uid", user.uid);
+
+      // Redirect to another page after successful login
+      window.location.href = "MenuPrincipal/menuprincipal.html";
+    })
+    .catch(function (error) {
+      // Login error
+      console.log("Login error:", error.code, error.message);
+      alert("Inicio de sesión fallido. Verifique sus credenciales.");
+    });
+}
+
+// Add an event listener to the login button
+document.getElementById("login-btn").addEventListener("click", loginUser);
