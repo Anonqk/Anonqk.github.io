1e0bdd9d4f07c421c64c22c3a4f53da5d34ebd8d
Sebastian Castillo <129561367+sebasZo@users.noreply.github.com>
2023-07-07 03:27:06 -0400

    ...

--- AreaAdministracion/scriptAdmin.js
+++ AreaAdministracion/scriptAdmin.js
@@ -13,3 +13,52 @@ file.addEventListener('change', e => {
     img.src = defaultFile;
   }
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrzCRyT3S-SY7Vvh58jkPtstVSLMvsStQ",
+  authDomain: "gestorinventarios-e37f6.firebaseapp.com",
+  databaseURL: "https://gestorinventarios-e37f6-default-rtdb.firebaseio.com",
+  projectId: "gestorinventarios-e37f6",
+  storageBucket: "gestorinventarios-e37f6.appspot.com",
+  messagingSenderId: "968579423727",
+  appId: "1:968579423727:web:04d75d7fda53cfd8086a1a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var fileText = document.querySelector(".fileText");
+var uploadPercentage = document.querySelector(".uploadPercentage")
+var progress = document.querySelector(".progress");
+var percentVal;
+var fileItem;
+var fileName;
+
+function getFile(e){
+  fileItem = e.target.files[0];
+  fileName = fileItem.name;
+  fileText.innerHTML = fileName;
+}
+
+function uploadImage(){
+  let storageRef = firebase.storage().ref("images/"+fileName);
+  let uploadTask = storageRef.put(fileItem);
+
+  uploadTask.on("state_changed", (snapshot) => {
+    console.log(snapshot);
+    percentVal = Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100);
+    console.log(percentVal);
+    uploadPercentage.innerHTML = percentVal+"%";
+    progress.style.width = percentVal+"%";
+  },(error) =>{
+    console.log("Error is ", error);
+  },()=>{
+    uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+      console.log("URL", url)
+
+      if(url != ""){
+        img.setAttribute("scr",url);
+        img.style.display="block";
+      }
+    }) 
+  })
+}
