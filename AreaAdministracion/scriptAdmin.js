d3db77952f6e7a9041f6e63a4ec7a3b5ab660afc
Sebastian Castillo <129561367+sebasZo@users.noreply.github.com>
2023-07-07 02:52:00 -0400

    ..

--- AreaAdministracion/scriptAdmin.js
+++ AreaAdministracion/scriptAdmin.js
@@ -1,14 +1,3 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDrzCRyT3S-SY7Vvh58jkPtstVSLMvsStQ",
-  authDomain: "gestorinventarios-e37f6.firebaseapp.com",
-  databaseURL: "https://gestorinventarios-e37f6-default-rtdb.firebaseio.com",
-  projectId: "gestorinventarios-e37f6",
-  storageBucket: "gestorinventarios-e37f6.appspot.com",
-  messagingSenderId: "968579423727",
-  appId: "1:968579423727:web:04d75d7fda53cfd8086a1a"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -27,25 +16,3 @@ file.addEventListener('change', e => {
     img.src = defaultFile;
   }
 });
-
-function uploadImage() {
-  const ref = firebase.storage().ref();
-  const file = document.querySelector("#photo").files(0);
-  const name = new Date() + '-' + file.name;
-  if (file == null) {
-    alert('Debe seleccionar una imagen')
-  } else {
-    const metadata = {
-      contentType: file.type
-    }
-    const task = ref.child(name).put(file, metadata);
-    task
-      .them(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        console.log(url);
-        alert('Imagen upload successful');
-        const imageElement = document.querySelector('#img');
-        imageElement.src = url;
-      })
-  }
-}
